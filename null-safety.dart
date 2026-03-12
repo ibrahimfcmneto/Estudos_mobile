ea8527d072182c20d164cb27e3704591b2ca3ce7
ibrahimfcmneto <ibrahim.fcmn@puccampinas.edu.br>
2026-03-12 01:34:18 -0300

    null safety

---
+++ null-safety.dart
@@ -0,0 +1,12 @@
+void main() {
+  
+  String? nome; //quando tem o "?" está forçando a acessar valores nulos
+  
+  nome = "ABC";
+  
+  print(nome!);
+  
+  late String sobrenome;
+  print(sobrenom);
+  
+}
