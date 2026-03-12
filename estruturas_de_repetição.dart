1f08013ac8c8fc5aa5278329b7834c0226af7f68
ibrahimfcmneto <ibrahim.fcmn@puccampinas.edu.br>
2026-03-12 07:58:25 -0300

    estruturas de repetição

---
+++ estruturas_de_repetição.dart
@@ -0,0 +1,19 @@
+void main()
+{
+  print("Tabuada do 5");
+
+  /*for (int i=0; i<10; i++)
+  {
+    print("5 x $i = ${5*i}");
+  }
+  */
+
+  //while
+  int i = 0;
+  while(i != 10)
+  {
+    print("$i");
+    i++;
+  }
+
+}
