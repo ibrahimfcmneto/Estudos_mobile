c66a083c5738d240b86550be78bbb8fb410c5c03
ibrahimfcmneto <ibrahim.fcmn@puccampinas.edu.br>
2026-03-12 01:34:08 -0300

    estruturas de fluxo if e else

---
+++ estruturas_de_fluxo.dart
@@ -0,0 +1,33 @@
+void main() {
+  //if / else
+
+  bool seguir_em_frente = false;
+  
+  if (seguir_em_frente)
+  {
+    print("Andar");
+  }
+  else
+    print("Parar");
+  
+  //switch
+  
+  int valorInt = 12;
+  
+  switch(valorInt)
+  {
+    case 0:
+      print("ZERO");
+      break;
+    case 1:
+      print("UM");
+      break;
+    case 2:
+      print("DOIS");
+      break;
+    default:
+      print("Novo valor");
+     
+  }
+  
+}
